4e847be03af929079f80b8bc607b2abc353d39d8
Ishita <ishitaarora382@gmail.com>
2022-08-08 15:44:35 +0530

    modified:   src/Components/Buttons.js 	modified:   src/Components/Cards.js

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -10,7 +10,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { styled } from "@mui/material/styles";
-import { color } from "@mui/system";
+
 
 const style = {
   position: 'absolute',
